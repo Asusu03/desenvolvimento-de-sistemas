d9d15776f8569d425f359056d2733d48449ca651
Asusu03 <suellenananias6@gmail.com>
2026-06-16 12:57:56 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,6 @@
+numero1 = float(input("digite o numero: "))
+numero2 = float(input("digite o numero: "))
+
+soma = numero1 + numero2 
+
+print("A soma é:", soma)
---
+++ aula4.py
@@ -0,0 +1,5 @@
+distancia = float(input("Digite a distancia percorrida: "))
+tempo = float(input("Digite o tempo gasto: "))
+
+velocidade = distancia / tempo 
+print("A velocidade é:", velocidade)
